8200b197b25428c0cfbd467b94ba6ffca33e6c95
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-11 11:55:43 -0500

    Create Settings.java

---
+++ Settings.java
@@ -0,0 +1,11 @@
+public class Settings {
+        public void setSpeed() {
+                
+        }
+        public void loadMap() {
+                
+        }
+        public void setMap() {
+                
+        }
+}
