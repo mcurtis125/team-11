35ee78860c9570d7681aaf17627b364cc7095214
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-11 11:56:57 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+
+
+        private int roundsWon;
+        private String username;
+        
+        public void readCSV(){
+                
+        }
+        
+        public void updateCSV(){
+                
+        }
+}
