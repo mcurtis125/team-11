febb4d7d2fa0693f085c0cdd81ec508221b1ce8b
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-18 21:41:58 -0500

    added methods for statistics

--- CSV.java
+++ CSV.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 /* CSV file format "username,password,wins,total games" */
 public class CSV {
 	
 	String filePath = "Database.csv"; 
+	private final int USERNAME_INDEX = 0;
+	private final int WINS_INDEX = 2;
+	private final int GAMES_INDEX = 3;
+	private String[][] statsArray;
 	public CSV() {
 		
 	}
@@ -42,8 +47,15 @@ public class CSV {
 		return false;
 	}
 	
-	public void addAccount(String username, String password) {
-	
+	public boolean addAccount(String username, String password) {
+		
+		if (verifyAccount(username, "")) {
+			return false;
+		}
+		if (password.length() < 8) {
+			return false;
+		}
+
 		try{
     		String data = username + "," + password + "," + 0 + "," + 0 + "\n";
  
@@ -65,7 +77,48 @@ public class CSV {
     	}catch(IOException e){
     		e.printStackTrace();
     	}
+		return true;
 	}
 	
-	
+	//method to return stats from Database.csv
+	public String[][] generateStats() {
+		BufferedReader reader = null;
+		try {
+		    File file = new File(filePath);
+		    reader = new BufferedReader(new FileReader(file));
+
+		    String line;
+			int numLines = 0; 
+			String seperator = ",";
+			ArrayList<String[]> statsList = new ArrayList<String[]>();
+			//read each line and store as seperated string array in arraylist
+		    while ( (line = reader.readLine() ) != null) {
+				statsList.add(line.split(seperator));
+				numLines++;
+		    }
+			//create and fill 2d array, length - 1 to exclude password information
+			String[][] statsArray = new String[numLines][statsList.get(0).length - 1];
+			//fill each row
+			for(int i = 0; i < statsArray.length; i++) {
+				//fill each column, exluding password information
+				statsArray[i][0] = statsList.get(i)[USERNAME_INDEX];
+				statsArray[i][1] = statsList.get(i)[WINS_INDEX];
+				statsArray[i][2] = statsList.get(i)[GAMES_INDEX];
+			}
+			return statsArray;
+
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        reader.close();
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
+		//tmp output
+		String[][] tmp = {{"file"}, {"could"},{"not be", "read"}};
+		return tmp;
+	}
 }
+
