e272270b8cedb8db3937975994619b0c27a51452
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-18 21:57:07 -0500

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Stats {
-        public void getAllRecords() {
-                
-        }
+	
+	private String[][] rowData;
+	private CSV csv;
+	
+	private final int WINS_COLUMN = 1;
+	
+	public Stats(CSV csv) {
+		this.csv = csv;
+		rowData = generateRowData();
+	}
+	
+	//getter for row data to be placed in jTable
+	public String[][] getRowData() {
+		return rowData;
+	}
+	
+	//method to generate and sort stats by most games won
+	public String[][] generateRowData() {
+		return bubbleSort( csv.generateStats() );
+	}
+	
+	//helpers to sort 2d data
+	public String[][] bubbleSort(String data[][]) {  
+		for(int i = data.length; i >= 2; i--) {
+			for(int j = 0; j < i - 1; j++) {
+				if( Integer.parseInt( data[j][WINS_COLUMN].trim() ) < Integer.parseInt( data[j+1][WINS_COLUMN].trim() ) ) { 
+					swap(data, j, j+1);
+				}
+			}
+		}
+		return data;
+	}  
+   
+	public void swap(String data[][], int index1, int index2) {  
+		String[] temp;  
+		temp = data[index1];  
+		data[index1] = data[index2];  
+		data[index2] = temp;  
+	}  
 }
