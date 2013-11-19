c9a062ed3c91af8b43cf3c0199c6615d56dd9d47
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-18 21:56:46 -0500

    Update StatisticsUI.java

--- StatisticsUI.java
+++ StatisticsUI.java
@@ -1,44 +1,54 @@
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.TableRowSorter;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
 
+
 public class StatisticsUI extends JPanel {
-	private JTable table;
-	private JTable table_1;
-	private StringFrame stringFrame;
+
+	private JTable dataTable;
+	private JScrollPane dataScroll;
+	private Control control;
+	private Stats stats;
 	
-	public StatisticsUI(StringFrame frame) {
-		this.stringFrame = frame;
+	private final String[] COLUMN_NAMES = {"Username:", "Number Of Games Won:", "Number Of Games Played:"};
+	
+	public StatisticsUI(Control controller, Stats stats) {
+		this.control = controller;
+		this.stats = stats;
 		setBackground(Color.CYAN);
 		setLayout(null);
 		
-		table = new JTable();
-		table.setBounds(225, 5, 0, 0);
-		add(table);
+		dataTable = new JTable(stats.getRowData(), COLUMN_NAMES);
+		dataTable.setEnabled(false);
+		dataScroll = new JScrollPane(dataTable);
 		
-		table_1 = new JTable();
-		table_1.setBounds(6, 140, 588, 619);
-		add(table_1);
+		dataScroll.setBounds(6, 140, 588, 619);	
+		dataTable.setBounds(6, 140, 588, 619);
+		//dataTable.setAutoCreateRowSorter(true);
+		add( dataScroll );
 		
 		JLabel lblStatistics = new JLabel("Statistics");
 		lblStatistics.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		lblStatistics.setBounds(251, 27, 78, 32);
 		add(lblStatistics);
 		
-		JButton btnLogin = new JButton("Login");
+		JButton btnLogin = new JButton("Back to Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				stringFrame.change("LOGIN");
+				control.change("LOGIN");
 				
 			}
 		});
 		btnLogin.setBounds(477, 73, 117, 21);
 		add(btnLogin);
 	}
+	
 }
